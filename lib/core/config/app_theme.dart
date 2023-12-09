ba847374a7dab355bdb1445508218c69939d4b94
Lemon1911 <m7moud.laymouna.ml@gmail.com>
2023-12-09 17:16:34 +0200

    ready for more code

--- lib/core/config/app_theme.dart
+++ lib/core/config/app_theme.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData appTheme = ThemeData(
-    primaryColor: const Color(0xff121312),
-  );
+      primaryColor: const Color(0xff121312),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Color(0xff121312),
+      ));
 }
